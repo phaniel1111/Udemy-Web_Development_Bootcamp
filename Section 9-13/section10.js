e03c70c3f85effe3aeef4e12d9bc3a8ad276450c
Phan Tấn Kiệt <75460679+phaniel1111@users.noreply.github.com>
2022-07-19 19:42:41 +0700

    Update section10.js

--- Section 9-13/section10.js
+++ Section 9-13/section10.js
@@ -14,3 +14,40 @@ function bmiCalculator(weight, height) {
     }
     return interpretation;
 }
+
+//Coding Exercise 6: Leap Year Challenge
+function isLeap(year) {
+    if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) {
+        return "Not leap year.";
+    }
+    return "Leap year.";
+}
+
+//Coding Exercise 7: Who's Buying Lunch? Code
+function whosPaying(names) {
+    var randomPerson = names[Math.floor(Math.random() * names.length)]
+    return randomPerson + " is going to buy lunch today!"
+}
+
+//Coding Exercise 8: The Fibonacci Exercise
+function fibonacciGenerator(n) {
+    var fib = [];
+
+    if (n === 0)
+        return fib;
+
+    if (n === 1 || n === 2) {
+        fib.push(0);
+        if (n === 2) {
+            fib.push(1)
+        }
+        return fib;
+    }
+
+    fib.push(0);
+    fib.push(1);
+    for (var i = 2; i < n; i++) {
+        fib.push(fib[i - 2] + fib[i - 1]);
+    }
+    return fib;
+}
